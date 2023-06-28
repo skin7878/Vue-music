b9069324282b60f638c3b7866eef8604e042b6bc
Igor Shokalo <igor.shokalo78@gmail.com>
2023-06-28 12:07:33 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h2>Hello world!</h2>
+  <h3 class="text-emerald-400">Hello world!</h3>
+</template>
+
+<style scoped></style>
